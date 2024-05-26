5c45c7f4a2a6ce92dbfa62cb1f4b8efb6d71fa30
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2024-05-26 09:16:11 +0600

    Update page.tsx

--- app/(root)/payment/page.tsx
+++ app/(root)/payment/page.tsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const page = () => {
   return (
     <div>
       <div className="flex items-center gap-x-10 justify-center">
-        <Image
-          className="cursor-pointer"
-          src="/images/visa.png"
-          alt="Visa Card Logo"
-          height={200}
-          width={300}
-        />
-        <Image
-          className="cursor-pointer"
-          src="/images/paypal.png"
-          alt="Visa Card Logo"
-          height={200}
-          width={300}
-        />
+        <Link href="https://sandbox.sslcommerz.com/EasyCheckOut/testcde64b91991e96050646de795c45a368adb">
+          <Image
+            className="cursor-pointer"
+            src="/images/visa.png"
+            alt="Visa Card Logo"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link href="https://sandbox.sslcommerz.com/EasyCheckOut/testcde64b91991e96050646de795c45a368adb">
+          <Image
+            className="cursor-pointer"
+            src="/images/paypal.png"
+            alt="Visa Card Logo"
+            height={200}
+            width={300}
+          />
+        </Link>
       </div>
     </div>
   );
